33ad95ffe72a3a5e95ee02ea772dc8363f92d3f6
Michael <michael.m.liendo.r@gmail.com>
2021-01-18 22:31:00 -0400

    se agrego el comando meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,13 +1,21 @@
+const Discord = require("discord.js");
+
 module.exports = {
     nombre: "meme",
     alias: ['memes'],
     description: "Envía un meme",
     run: (client, message, args) => {
         const meme = require('../../utils/memes.json')
+        const value = Math.floor(Math.random() * meme.length)
 
-        let value = Math.floor(Math.random() * meme.length)
 
-        message.channel.send(meme[value])
+        const embed = new Discord.MessageEmbed()
+        .setTitle('New meme')
+        .setImage(meme[value])
+        .setTimestamp()
+        .setColor(0xfff)
+
+        message.channel.send(embed)
 
     }
 }
