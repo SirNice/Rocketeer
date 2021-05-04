652375216f43b302c8590d7433ab5207f48ee9ed
Michael <>
2021-05-04 19:32:24 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// ===== SERVIDOR ===== //
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () =>
-	console.log(`Example app listening at http://localhost:${port}`)
-); 
 
 // ==== REQUIRIMIENTOS ===== //
 
@@ -31,34 +21,34 @@ client.eventos = new Discord.Collection()
 
 
 // ======= COMANDOS ======= //
-for(const subcarpet of fs.readdirSync(__dirname + "/commands/")){ 
+for (const subcarpet of fs.readdirSync(__dirname + "/commands/")) {
 
-    for(const file of fs.readdirSync(__dirname +`/commands/${subcarpet}`)){
+  for (const file of fs.readdirSync(__dirname + `/commands/${subcarpet}`)) {
 
-        if(file.endsWith(".js")){
-            let fileContents = require(`./commands/${subcarpet}/${file}`);
-            let fileName = fileContents.nombre;
-            client.comandos.set(fileName, fileContents);
-            comandosHandler++
-        }
-        
+    if (file.endsWith(".js")) {
+      let fileContents = require(`./commands/${subcarpet}/${file}`);
+      let fileName = fileContents.nombre;
+      client.comandos.set(fileName, fileContents);
+      comandosHandler++
     }
+
+  }
 
 }
 
 // ======== EVENTOS ======== //
 
-for(const subcarpet of fs.readdirSync( __dirname + '/events/')){
+for (const subcarpet of fs.readdirSync(__dirname + '/events/')) {
 
-    for(const file of fs.readdirSync(__dirname + `/events/${subcarpet}`)){
-        if(file.endsWith(".js")){
-            let fileName = file.substring(0, file.length - 3);
-            let fileContents = require(`./events/${subcarpet}/${file}`);
-            client.on(fileName, fileContents.bind(null, client));
-            delete require.cache[require.resolve(`./events/${subcarpet}/${file}`)];
-            eventosHandler++
-        } 
+  for (const file of fs.readdirSync(__dirname + `/events/${subcarpet}`)) {
+    if (file.endsWith(".js")) {
+      let fileName = file.substring(0, file.length - 3);
+      let fileContents = require(`./events/${subcarpet}/${file}`);
+      client.on(fileName, fileContents.bind(null, client));
+      delete require.cache[require.resolve(`./events/${subcarpet}/${file}`)];
+      eventosHandler++
     }
+  }
 }
 // ==== =============== ==== //
 
@@ -66,7 +56,7 @@ console.log(`Se han cargado ${comandosHandler} comandos, y ${eventosHandler} eve
 
 
 // ==== =============== ==== //
-let db = require('./database/connect')
-db()
+require('./database/connect')();
 
+require('./server')();
 client.login(process.env.DISCORD_TOKEN);
